a798e582babbb6d5bb5419d2b0af55a11fa7b04d
Saipriya <saipriya.innovative@gmail.com>
2023-05-07 23:55:57 +0530

    Simple Hello World RESTFUL WEB SERVICES

---
+++ spring_learning/restful-web-services/src/main/java/learn/restapi/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,30 @@
+package learn.restapi.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// REST API
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	// these variables are called Path Parameters
+	// /users/{id}/todos/{id}  => /users/1/todos/1
+	// /hello-world/path-variable/{name}
+	// /hello-world/path-variable/Ranga
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		//return new HelloWorldBean("Hello World " + name);
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
