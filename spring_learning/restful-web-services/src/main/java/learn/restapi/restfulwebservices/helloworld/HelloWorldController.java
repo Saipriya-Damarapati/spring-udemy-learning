d3a9a71758828033cadf0749505192af9cd63ca3
Saipriya <saipriya.innovative@gmail.com>
2023-05-08 22:15:09 +0530

    Advanced REST API features - phase 1

--- spring_learning/restful-web-services/src/main/java/learn/restapi/restfulwebservices/helloworld/HelloWorldController.java
+++ spring_learning/restful-web-services/src/main/java/learn/restapi/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package learn.restapi.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -26,5 +36,11 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		//return new HelloWorldBean("Hello World " + name);
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-i18N")
+	public String helloWorldI18N() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
